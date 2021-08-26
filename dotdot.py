b2807e52a8a124e60d8efe707b3c883f26ce5871
Nick Brown <bicknrown@gmail.com>
2021-08-26 11:48:34 -0400

    created basic layout of routes further definition of routes required, thanks emil.

--- dotdot.py
+++ dotdot.py
@@ -7,3 +7,27 @@ dotdot = Flask(__name__)
 @dotdot.route("/")
 def test():
     return "<p>Hello, World!</p>"
+
+@dotdot.route("/print")
+def print():
+    return "stuff"
+
+@dotdot.route("/print/test")
+def print_test():
+    return "stuff"
+
+@dotdot.route("/print/raw")
+def print_raw():
+    return "stuff"
+
+@dotdot.route("/print/ascii")
+def print_ascii():
+    return "stuff"
+
+@dotdot.route("/print/image")
+def print_image():
+    return "stuff"
+
+@dotdot.route("/bell")
+def bell():
+    return "stuff"
