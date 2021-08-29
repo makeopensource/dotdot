a671c41c5300f17750bac67659408ca980e6337f
Emil Kovacev <emilkova@buffalo.edu>
2021-08-29 12:16:24 -0400

    configured base ascii functionality

--- dotdot.py
+++ dotdot.py
@@ -1,45 +1,56 @@
 from flask import Flask
 import flask
 from dotenv import load_dotenv
-import text_to_font as ttf
-import toprint
+from text_to_font.transformations import word_to_ascii
+import toprint as tp
 
 load_dotenv()
 dotdot = Flask(__name__)
+
+
+class CharacterLimitExceeded(Exception):
+    """Printer exceeded line width of 80 characters"""
+    pass
+
 
 @dotdot.route("/")
 def test():
     return "<p>Hello, World!</p>"
 
-@dotdot.route("/print", methods=['GET', 'POST'])
-def print():
-    if flask.request.method == 'POST':
-        content = flask.request.json.get('content')
-        sanitized = toprint.sanitize(content)
-        sanlist = toprint.eightyCharChunk(sanitized)
-        for sanstring in sanlist:
-            toprint.toprint(sanstring)
-    return "stuff"
+@dotdot.route("/print", methods=['POST'])
+def print(response):
+    content = flask.request.json.get('content')
+    sanitized = tp.sanitize(content)
+    sanlist = tp.eightyCharChunk(sanitized)
+    for sanstring in sanlist:
+        tp.toprint(sanstring)
+    return response.status
 
-@dotdot.route("/print/test")
-def print_test():
-    return "stuff"
+@dotdot.route("/print/test", methods=['POST'])
+def print_test(response):
+    tp.toprint('hello world!')
+    return response.status
 
-@dotdot.route("/print/raw", methods=['GET', 'POST'])
-def print_raw():
-    if flask.request.method == 'POST':
-        content = flask.request.json.get('content')
-        toprint.toprint(content)
-    return "stuff"
+@dotdot.route("/print/raw", methods=['POST'])
+def print_raw(response):
+    content = flask.request.json.get('content')
+    tp.toprint(content)
+    return response.status
 
-@dotdot.route("/print/ascii")
-def print_ascii():
-    return "stuff"
+@dotdot.route("/print/ascii", methods=['POST'])
+def print_ascii(response):
+    content = flask.request.json.get('content')
+    sanitized = tp.sanitize(content).split('\n')
+    for sanstring in sanitized:
+        if len(sanstring) > 65:
+            raise CharacterLimitExceeded
+        tp.toprint(sanstring)
+    return response.status
 
 @dotdot.route("/print/image")
-def print_image():
+def print_image(response):
     return "stuff"
 
 @dotdot.route("/bell")
-def bell():
-    return "stuff"
+def bell(response):
+    return "ding"
